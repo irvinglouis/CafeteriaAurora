b73e359f7db2292afadd26d94995f7a792a0af5b
Irving Saavedra <contabilidad3@elremanso.com>
2026-09-10 14:44:46 -0500

    fix: configura deploy de Next.js en GitHub Pages

--- cafe-aurora-adm/next.config.mjs
+++ cafe-aurora-adm/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.GITHUB_ACTIONS ? '/CafeteriaAurora' : '',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
